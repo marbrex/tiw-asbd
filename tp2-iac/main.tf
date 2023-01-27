21ac4c3ca8f41743a2b2d82daf9a454de4f8190d
KASMAMYTOV ELDAR p1712650 <eldar.kasmamytov@etu.univ-lyon1.fr>
2023-01-27 11:42:52 +0100

    Fix allocation pool block for subnet

--- tp2-iac/main.tf
+++ tp2-iac/main.tf
@@ -7,7 +7,7 @@ resource "openstack_networking_subnet_v2" "asbd-subnet-front" {
   network_id      = "${openstack_networking_network_v2.network-asbd-edgerunner.id}"
   cidr            = "10.245.199.0/24"
   dns_nameservers = ["10.10.10.10", "10.10.10.11"]
-  allocation_pool = {
+  allocation_pool {
     start = "10.245.199.100"
     end   = "10.245.199.150"
   }
